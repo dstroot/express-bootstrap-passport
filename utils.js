087aa8e59a18a6f8c3d88a0d8127723031143693
Dan Stroot <dan@thestroots.com>
2013-01-01 11:48:11 -0800

    Now had to fix icons

--- utils.js
+++ utils.js
@@ -1,40 +1,21 @@
 'use strict'
 /* ==============================================================
-    Include required packages / Module Dependencies
-=============================================================== */
+ *  Include required packages / Module Dependencies
+ * ============================================================== */
 var crypto          = require('crypto');
 
-exports.smsify = function(str) {
-  if (str.length <= 160) { return str; }
-  else { return str.substr(0,157)+'...'; }
-};
-
-exports.initcap = function(str) {
-  return str.substring(0,1).toUpperCase() + str.substring(1);
-};
-
-exports.testint = function(str) {
-  var intRegex = /^\d+$/;
-  if(intRegex.test(str)) {
-    return true;
-  }
-  return false;
-};
-
-exports.formatPhone = function(phonenum) {
-    var regexObj = /^(?:\+?1[-. ]?)?(?:\(?([0-9]{3})\)?[-. ]?)?([0-9]{3})[-. ]?([0-9]{4})$/;
-    if (regexObj.test(phonenum)) {
-        var parts = phonenum.match(regexObj);
-        var phone = "";
-        if (parts[1]) { phone += "(" + parts[1] + ") "; }
-        phone += parts[2] + "-" + parts[3];
-        return phone;
-    }
-    else {
-        //invalid phone number
-        return phonenum;
-    }
-};
+/**
+ * Returns a hashed password
+ *
+ * Examples:
+ *
+ *   utils.hash('password', 'salt')
+ *
+ * @param  {string} pass
+ * @param  {string} salt
+ * @return {string}
+ * @api private
+ */
 
 exports.hash = function (pass, salt) {
   var h = crypto.createHash('sha512');
